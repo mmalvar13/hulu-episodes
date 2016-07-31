4aefc59c3e5f658a60d4199e4e8bbdf77bfc1a48
Monica Alvarez <mmalvar13@gmail.com>
2016-07-30 20:57:27 -0600

    Update Delete and Insert PDO

--- php/classes/Account.php
+++ php/classes/Account.php
@@ -1,13 +1,9 @@
 <?php
+namespace Edu\Cnm\malvarez30\HuluEpisodes;
 
-/**
- * Created by PhpStorm.
- * User: mmalv
- * Date: 7/23/2016
- * Time: 10:35 AM
- */
+require_once("autoload.php");
 
-class Account{
+class Account {
 	/**
 	 * id for this Account; this is the primary key
 	 * @var int $accountId
@@ -53,12 +49,12 @@ class Account{
 
 			//rethrow the exception to the caller
 			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
-		} catch(\RangeException $range){
+		} catch(\RangeException $range) {
 			//rethrow the exception to the caller
-			throw(new \RangeException($range->getMessage(),0,$range));
-		}catch(\Exception $exception){
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\Exception $exception) {
 			//rethrow the exception to the caller
-			throw(new \Exception($exception->getMessage(),0,$exception));
+			throw(new \Exception($exception->getMessage(), 0, $exception));
 		}
 	}
 
@@ -96,35 +92,35 @@ class Account{
 		$this->accountId = $newAccountId;
 	}
 
-/**
- * accessor method for accountEmail
- * @return string value of accountEmail
- **/
-public function getAccountEmail() {
-	return ($this->accountEmail);
-}
+	/**
+	 * accessor method for accountEmail
+	 * @return string value of accountEmail
+	 **/
+	public function getAccountEmail() {
+		return ($this->accountEmail);
+	}
 
-/**
- * mutator method for accountEmail
- * @param string $newAccountEmail new value of account Email
- * @throws /InvalidArgumentException if $newAccountEmail is not a string
- * @throws /RangeException if $newAccountEmail is > 120 characters
- * @throws /TypeError if $newAccountEmail is not a string (is invalid argument exception and type error the same in this case???)
- **/
-public function setAccountEmail(string $newAccountEmail) {
-	//verify that the account email entered is secure
-	$newAccountEmail = trim($newAccountEmail);
-	$newAccountEmail = filter_var($newAccountEmail, FILTER_SANITIZE_STRING);
-	if(empty($newAccountEmail) === true) {
-		throw(new \InvalidArgumentException ("account email is empty or insecure"));
-	}
-	//verify the account email will fit in the database
-	if(strlen($newAccountEmail) > 120) {
-		throw(new \RangeException ("account email has too many characters"));
-	}
+	/**
+	 * mutator method for accountEmail
+	 * @param string $newAccountEmail new value of account Email
+	 * @throws \InvalidArgumentException if $newAccountEmail is not a string
+	 * @throws \RangeException if $newAccountEmail is > 120 characters
+	 * @throws \TypeError if $newAccountEmail is not a string (is invalid argument exception and type error the same in this case???)
+	 **/
+	public function setAccountEmail(string $newAccountEmail) {
+		//verify that the account email entered is secure
+		$newAccountEmail = trim($newAccountEmail);
+		$newAccountEmail = filter_var($newAccountEmail, FILTER_SANITIZE_STRING);
+		if(empty($newAccountEmail) === true) {
+			throw(new \InvalidArgumentException ("account email is empty or insecure"));
+		}
+		//verify the account email will fit in the database
+		if(strlen($newAccountEmail) > 120) {
+			throw(new \RangeException ("account email has too many characters"));
+		}
 		//store the email content
 		$this->accountEmail = $newAccountEmail;
-		}//probably more exceptions for ensuring it's an actual email address like @foo.com or something
+	}//probably more exceptions for ensuring it's an actual email address like @foo.com or something
 
 	/**
 	 *accessor method for account Username
@@ -155,4 +151,69 @@ public function setAccountEmail(string $newAccountEmail) {
 		//store the account username
 		$this->accountUsername = $newAccountUsername;
 	}
+
+	/**
+	 * inserts this Account into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection to object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo){
+		//enforce the accountId is null (i.e. don't insert an account that already exists)
+		if($this->accountId !== null){
+			throw(new \PDOException("not a new account"));
+		}
+		//create query template
+		$query = "INSERT INTO account(accountEmail, accountHash, accountSalt, accountUsername) 
+		VALUES(:accountEmail, :accountHash, :accountSalt, :accountUsername)";
+		$statement = $pdo->prepare($query);
+		//bind the member variables to the place holders in the template
+		$parameters = ["accountEmail"=>$this->accountEmail, "accountHash"=>$this->accountHash,"accountSalt"=>$this->accountSalt,
+		"accountUsername"=>$this->accountUsername];
+		$statement->execute($parameters);
+
+		//update the null accountId with what mySQL just gave us
+		$this->accountId = intval($pdo->lastInsertId());
+	}
+	/**
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo) {
+		//enforce the accountId is not null (i.e. don't delete an account that has not been inserted)
+		if($this->accountId === null){
+			throw(new \PDOException("unable to delete a tweet that does not exist"));
+		}
+		//create query template
+		$query = "DELETE FROM account WHERE accountId = :accountId";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variables to the place holder in the template
+		$parameters = ["accountId" => $this->accountId];
+		$statement->execute($parameters);
+	}
+	/**
+	 * updates this Account in mySQL
+	 * @params \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update(\PDO $pdo){
+		//enforce the accountId is not null i.e., don't update an account that hasnt been inserted
+		if($this->accountId === null){
+			throw(new \PDOException("unable to update an account that does not exist"));
+		}
+
+		//create query template
+		$query = "UPDATE account SET accountEmail = :accountEmail, accountHash = :accountHash, accountSalt = :accountSalt, accountUsername = :accountUsername WHERE accountId = :accountId";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variables to the placeholders in the template
+		$parameters = ["accountId" => $this->accountId, "accountEmail"=>$this->accountEmail, "accountHash"=>$this->accountHash, "accountSalt"=>$this->accountSalt, "accountUsername"=>$this->accountUsername];
+
+		$statement->execute($parameters);
+	}
+
 }
--- php/classes/Series.php
+++ php/classes/Series.php
@@ -1,7 +1,76 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: mmalv
- * Date: 7/26/2016
- * Time: 10:07 AM
- */
+ * Class Series
+ **/
+
+class Series{
+	/**
+	 * the id for this Series. This is the primary key.
+	 * @var int $seriesId
+	 **/
+	private $seriesId;
+	/**
+	 * the name for this Series.
+	 * @var string $seriesName
+	 **/
+	private $seriesName;
+
+	/**
+	 * constructor for this Series
+	 * @param int $seriesId id of the series
+	 * @param string $seriesName name of the series
+	 * @throws \InvalidArgumentException if types are not valid
+	 * @throws \RangeException if data values are out of range
+	 * @throws \TypeError if type violates type hints
+	 *@throws \Exception if any other exception occurs
+	 **/
+	public function __construct(int $newSeriesId, string $newSeriesName) {
+		try{
+			$this->seriesId($newSeriesId);
+			$this->seriesName($newSeriesName);
+		}catch(\InvalidArgumentException $invalidArgument){
+			//rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(),0,$invalidArgument));
+		}catch(\RangeException $range) {
+			//rethrow exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}catch(\TypeError $typeError){
+			//rethrow exception to the caller
+			throw(new \TypeError($typeError->getMessage(),0,$typeError));
+		}catch(\Exception $exception){
+			//rethrow exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+	/**
+	 * accessor method for series id.
+	 * @return int value of series id
+	 **/
+	public function getSeriesId(){
+		return($this->seriesId);
+	}
+	/**
+	 * mutator method for series id
+	 * @param int $newSeriesId new value of series Id
+	 * @throws \RangeException if $newSeriesId is not positive
+	 * @throws \TypeError if $newSeriesId is not an integer
+	 **/
+	public function setSeriesId(){
+		if($newSeriesId <= 0){
+			throw(new \RangeException("series id is not positive"));
+		}
+		//convert and store the series id
+		$this->seriesId = $newSeriesId;
+	}
+	/**
+	 * accessor method for series Name
+	 * @param string $newSeriesName is the string value of the series Name
+	 * @throws \RangeException if $newSeriesName is > 128 characters
+	 * @throws \TypeError if $newSeriesName is not a string
+	 * @throws \Exception if another exception occurs
+	 **/
+	public function setSeriesName()
+		if($newSeriesName )
+	}
+}
